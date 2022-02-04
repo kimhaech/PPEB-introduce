79469cb02de69c7e24cba918b42fd5ba789c6384
김주원 <kjwt1124@icloud.com>
2022-02-04 21:58:16 +0900

    Update member page button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,5 @@
 module.exports = {
-    purge: {
-        // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-        enabled: process.env.NODE_ENV === 'production',
-        content: [
-          'components/**/*.vue',
-          'layouts/**/*.vue',
-          'pages/**/*.vue',
-          'plugins/**/*.js',
-          'nuxt.config.js'
-        ]
-      },
+    purge:[],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
