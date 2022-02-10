4ae95efe825da216b717021c41d4e2495c7e62bd
SeongilHeo <tjddlf101@gmail.com>
2022-02-10 17:16:09 +0900

    Fix Main Banner
    Add Logo to Banner
    Avoid changing Banner size

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import ParticlesBg from 'particles-bg';
 import Typewriter from '../../modules/Typewriter/Typewriter';
-
+import bannerlogo from '../../static/images/logo_black.png';
 const Main = () => {
     return (
         <section className="text-gray-700 body-font bg-transparent relative">
             <ParticlesBg type="cobweb" bg={true} />
-            <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-                <div className="mt-52 lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                    <div className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+            <div className="container mx-auto flex md:px-5 md:py-24 pt-32 pb-10 md:flex-row flex-col-reverse items-center">
+                <div className="md:mt-52 my-1 xl:pl-32 md:pl-1 lg:flex-grow md:w-1/2 flex flex-col md:items-start md:text-left md:mb-16 md:mb-0 items-center text-center">
+                    <div className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 h-20">
                         <Typewriter
                             options={{
                                 strings: [
@@ -47,6 +47,13 @@ const Main = () => {
                             </svg>
                         </button>
                     </div> */}
+                </div>
+                <div className='xl:pt-8 xl:pl-1 lg:w-1/3 lg:pt-24 lg:pl-4 md:w-1/2 md:pt-40 md:pl-32 pt-12 pb-4'>
+                    <img
+                        class="xl:h-80 lg:h-64 md:h-52 sm:h-40 h-32"
+                        src={bannerlogo}
+                        alt="bannerlogo"
+                    />
                 </div>
             </div>
         </section>
