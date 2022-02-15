2433ada1d474f684972feb5d84f520114e4d3099
haechi <42764085+kimhaech@users.noreply.github.com>
2022-02-15 14:04:14 +0900

    select and title fix

--- src/pages/Achievement/Achievements.js
+++ src/pages/Achievement/Achievements.js
@@ -16,11 +16,41 @@ const Achievements = () => {
     })
   }
 
+  // mobile --> mt-36, pc --> mt-16 수정
+  // mobile 글자크기, 한줄 나열
+
   let ye = Object.keys(Yearslist).sort().reverse()
   return (
     <div>
       <Header />
-      <div class="mt-24">
+      <div class='fixed inset-x-0 top-0 left-0 z-50 right-0'>
+      <div class="bg-white p- mt-16">
+        <div className="flex flex-col text-center w-full">
+          <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
+            Achievements Page
+          </h2>
+          <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">
+            활동성과
+          </h1>
+        </div>
+      
+    <div class="flex flex-col items-center">
+      <select class="mx-10 text-center"
+        onChange={FocusOn}
+      >
+        <option selected disabled>
+          년도 선택
+        </option>
+
+        {ye.map((year, index) => (
+          <option>{year}</option>
+        ))}
+      </select>
+      </div>
+      </div>  
+      </div>
+      <div class="pt-32"></div>
+      {/* <div class="bg-red-100 p-5 mt-16">
         <div className="flex flex-col text-center w-full">
           <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
             Achievements Page
@@ -30,7 +60,6 @@ const Achievements = () => {
           </h1>
         </div>
       </div>
-
     <div class="flex flex-col items-center">
       <br></br>
       <select class="fixed mx-10 text-center z-50"
@@ -44,7 +73,7 @@ const Achievements = () => {
           <option>{year}</option>
         ))}
       </select>
-      </div>
+      </div> */}
       <br></br>
       <AchieveLists />
       <Footer />
