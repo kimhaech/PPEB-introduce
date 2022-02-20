b377ab94e718582e2940fc63337b7201c6603e93
SeongilHeo <tjddlf101@gmail.com>
2022-02-20 17:45:22 +0900

    fix: achievement line

--- src/pages/Achievement/AchieveLists.js
+++ src/pages/Achievement/AchieveLists.js
@@ -22,9 +22,9 @@ const AchieveLists = () => {
   return (
     <div class="container bg-white-200 mx-auto w-full h-full">
       <div class="relative wrap overflow-hidden p-10 h-full">
-        <div class="border-2-2 absolute border-opacity-20 border-gray-700 h-full border left-1/2"></div>
+        <div class="border-2-2 absolute border-opacity-20 border-gray-700 h-full border left-1/2 z-0"></div>
         {y_li.map((year, index) => (
-          <div id="{year}">
+          <div id="{year}" class="relative z-50">
             <Showachieve year={year} index={index}></Showachieve>
           </div>
         ))}
